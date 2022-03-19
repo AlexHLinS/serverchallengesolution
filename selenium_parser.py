a7fbb3428b77f85c1f97c94a13095c58e2a8a4b8
Alex Shkil <alexhlins@gmail.com>
2022-03-19 19:44:16 +0300

    readme update

--- selenium_parser.py
+++ selenium_parser.py
@@ -4,6 +4,7 @@ import pandas as pd
 
 PATH_TO_CHROME_DRIVER = '/usr/lib/chromium-browser/chromedriver'
 
+
 def get_company_list_by_product_metalloprokat(product_name):
     # парсер www.metalloprokat.ru
 
@@ -18,19 +19,21 @@ def get_company_list_by_product_metalloprokat(product_name):
     driver.get(TARGET_URL+product_name)
     site_raw = driver.page_source
     parsed = bs(site_raw, features='lxml')
-    
-    comp_count = int(parsed.find_all('div', {'class': 'hcl-tip'})[0].contents[0].split()[5])
+
+    comp_count = int(parsed.find_all(
+        'div', {'class': 'hcl-tip'})[0].contents[0].split()[5])
 
     if comp_count < 1:
         return sellers
 
-    companies = parsed.find_all('span', {'class': 'cii-pseudo-link js-ykr-action js-ga-link js-catalogue-ecommerce js-encrypted-seo-link'})
+    companies = parsed.find_all('span', {
+                                'class': 'cii-pseudo-link js-ykr-action js-ga-link js-catalogue-ecommerce js-encrypted-seo-link'})
 
     for company in companies:
         try:
             seller_name = company.contents[0]
             sellers[seller_name] = []
-            #sellers[seller_name].append(company.find_all('a')[0]['href'])
+            # sellers[seller_name].append(company.find_all('a')[0]['href'])
         except:
             pass
 
@@ -46,7 +49,7 @@ def get_company_list_by_product_metalloprokat(product_name):
     driver.close()
     for seller in sellers:
         try:
-            #raw_for_inn = requests.get(
+            # raw_for_inn = requests.get(
             #    GET_INN_URL+seller+GET_INN_URL_POSFIX, headers=headers)
             driver = webdriver.Chrome(PATH_TO_CHROME_DRIVER)
             driver.implicitly_wait(2)
@@ -54,7 +57,7 @@ def get_company_list_by_product_metalloprokat(product_name):
             raw_for_inn = driver.page_source
             parsed = bs(raw_for_inn, features='lxml')
             inn = parsed.find_all('div', {'class': 'serp-list serp-list_right_yes serp-list_complementary_yes'})[
-            0].find_all('div', {'class': 'KeyValue-ItemValue'})[0].text
+                0].find_all('div', {'class': 'KeyValue-ItemValue'})[0].text
             '''company = parsed.find_all('div', {'class': 'company-item'})[0]
             company_info = company.find_all(
                 'div', {'class': 'company-item-info'})[1]
@@ -78,8 +81,8 @@ def get_company_list_by_product(product_name):
     return result
 
 
-
 # display = Display(visible=0, size=(1600, 1200))
 # display.start()
-print(get_company_list_by_product('* Гвоздь строительный круглый головка плоская 3х80 ГОСТ 4028'))
+print(get_company_list_by_product(
+    '* Гвоздь строительный круглый головка плоская 3х80 ГОСТ 4028'))
 # display.stop()
